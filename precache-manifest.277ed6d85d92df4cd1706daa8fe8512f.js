d11bef2f90b2c9d6fc6a48308a0124db536c78f3
chanjessica <jchan_sea@yahoo.com>
2020-06-02 15:22:14 -0700

    Updates

---
+++ precache-manifest.277ed6d85d92df4cd1706daa8fe8512f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dff5fe07a2b40210204ee4198cc271c",
+    "url": "/week5-assignment/index.html"
+  },
+  {
+    "revision": "04341a6495707e579985",
+    "url": "/week5-assignment/static/css/main.e9638b32.chunk.css"
+  },
+  {
+    "revision": "751080583aea6080b67f",
+    "url": "/week5-assignment/static/js/2.49b8c520.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/week5-assignment/static/js/2.49b8c520.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04341a6495707e579985",
+    "url": "/week5-assignment/static/js/main.40c09dda.chunk.js"
+  },
+  {
+    "revision": "84cba6f59a0aa7c46a74",
+    "url": "/week5-assignment/static/js/runtime-main.3d0d46f0.js"
+  }
+]);
